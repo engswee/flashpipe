6bfe169620519e5c642afefc59e1fff7861c666b
engswee <engswee@users.noreply.github.com>
2021-05-21 21:54:23 +0200

    Optional parameters to set delay and limit for deployment
    When not set:
    Delay defaults to 30 seconds
    Max limit defaults to 10 times

--- src/main/docker/script/deploy_runtime_artifact.sh
+++ src/main/docker/script/deploy_runtime_artifact.sh
@@ -6,7 +6,7 @@ function die() {
 }
 
 function usage() {
-  echo -e "usage: deploy.sh [--logcfgfile=<pathtofile>] iflow_id tmn_host cpi_user cpi_password\n"
+  echo -e "usage: deploy_runtime_artifact.sh [--logcfgfile=<pathtofile>] [--delay=<delay_in_sec>] [--maxcheck=<count>] [--classpath_base_dir=<path_to_dir>] iflow_id tmn_host cpi_user cpi_password\n"
 }
 
 logcfgfile=
@@ -22,6 +22,24 @@ while :; do
     ;;
   --logcfgfile=) # Handle the case of an empty --logcfgfile=
     die 'ERROR: "--logcfgfile" requires a non-empty option argument.'
+    ;;
+  --classpath_base_dir=?*)
+    classpath_base_dir=${1#*=} # Delete everything up to "=" and assign the remainder.
+    ;;
+  --classpath_base_dir=) # Handle the case of an empty --classpath_base_dir=
+    die 'ERROR: "--classpath_base_dir" requires a non-empty option argument.'
+    ;;
+  --delay=?*)
+    delay=${1#*=} # Delete everything up to "=" and assign the remainder.
+    ;;
+  --delay=) # Handle the case of an empty --delay=
+    die 'ERROR: "--delay" requires a non-empty option argument.'
+    ;;
+  --maxcheck=?*)
+    maxcheck=${1#*=} # Delete everything up to "=" and assign the remainder.
+    ;;
+  --maxcheck=) # Handle the case of an empty --maxcheck=
+    die 'ERROR: "--maxcheck" requires a non-empty option argument.'
     ;;
   --) # End of all options.
     shift
@@ -42,13 +60,40 @@ iflow_id=$1
 tmn_host=$2
 cpi_user=$3
 cpi_password=$4
+if [ -z "$delay" ]; then
+  delay_in=30
+else
+  delay_in=$delay
+fi
+if [ -z "$maxcheck" ]; then
+  maxcheck_in=10
+else
+  maxcheck_in=$maxcheck
+fi
+
+if [ -z "$classpath_base_dir" ]; then
+  source /usr/bin/set_classpath.sh
+else
+  echo "[INFO] Using $classpath_base_dir as classpath base directory "
+  echo "[INFO] Setting WORKING_CLASSPATH environment variable"
+  #  FLASHPIPE_VERSION
+  export WORKING_CLASSPATH=$classpath_base_dir/repository/com/equalize/flashpipe/1.0.1/flashpipe-1.0.1.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/codehaus/groovy/groovy-all/2.4.12/groovy-all-2.4.12.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/apache/httpcomponents/core5/httpcore5/5.0.4/httpcore5-5.0.4.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/apache/httpcomponents/client5/httpclient5/5.0.4/httpclient5-5.0.4.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/commons-codec/commons-codec/1.15/commons-codec-1.15.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/slf4j/slf4j-api/1.7.25/slf4j-api-1.7.25.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/apache/logging/log4j/log4j-slf4j-impl/2.14.1/log4j-slf4j-impl-2.14.1.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/apache/logging/log4j/log4j-api/2.14.1/log4j-api-2.14.1.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/apache/logging/log4j/log4j-core/2.14.1/log4j-core-2.14.1.jar
+  export WORKING_CLASSPATH=$WORKING_CLASSPATH:$classpath_base_dir/repository/org/zeroturnaround/zt-zip/1.14/zt-zip-1.14.jar
+fi
 
 echo "[INFO] Deploying design time IFlow $iflow_id to tenant runtime"
-source /usr/bin/set_classpath.sh
 if [ -z "$logcfgfile" ]; then
-  echo "[INFO] Executing command: java -classpath $WORKING_CLASSPATH com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact $iflow_id $tmn_host $cpi_user $cpi_password"
-  java -classpath $WORKING_CLASSPATH com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact $iflow_id $tmn_host $cpi_user $cpi_password
+  echo "[INFO] Executing command: java -classpath $WORKING_CLASSPATH com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact $iflow_id $tmn_host $cpi_user $cpi_password $delay_in $maxcheck_in"
+  java -classpath "$WORKING_CLASSPATH" com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact "$iflow_id" "$tmn_host" "$cpi_user" "$cpi_password" $delay_in $maxcheck_in
 else
-  echo "[INFO] Executing command: java -Dlog4j.configurationFile=$logcfgfile -classpath $WORKING_CLASSPATH com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact $iflow_id $tmn_host $cpi_user $cpi_password"
-  java -Dlog4j.configurationFile=$logcfgfile -classpath $WORKING_CLASSPATH com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact $iflow_id $tmn_host $cpi_user $cpi_password
+  echo "[INFO] Executing command: java -Dlog4j.configurationFile=$logcfgfile -classpath $WORKING_CLASSPATH com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact $iflow_id $tmn_host $cpi_user $cpi_password $delay_in $maxcheck_in"
+  java -Dlog4j.configurationFile="$logcfgfile" -classpath "$WORKING_CLASSPATH" com.equalize.flashpipe.cpi.exec.DeployDesignTimeArtifact "$iflow_id" "$tmn_host" "$cpi_user" "$cpi_password" $delay_in $maxcheck_in
 fi
--- src/main/groovy/com/equalize/flashpipe/cpi/exec/DeployDesignTimeArtifact.groovy
+++ src/main/groovy/com/equalize/flashpipe/cpi/exec/DeployDesignTimeArtifact.groovy
@@ -6,7 +6,7 @@ import com.equalize.flashpipe.cpi.api.RuntimeArtifact
 import java.util.concurrent.TimeUnit
 
 if (args.length < 4) {
-    println "Enter arguments in the format: <iflow_id> <cpi_host> <user> <password>"
+    println "Enter arguments in the format: <iflow_id> <cpi_host> <user> <password> <delay_length> <max_check_limit>"
     System.exit(1)
 }
 
@@ -14,15 +14,20 @@ def iFlowId = args[0]
 def host_tmn = args[1]
 def user = args[2]
 def pw = args[3]
+int delayLength = (args.length > 4) ? args[4] as int : 30
+int maxCheckLimit = (args.length > 5) ? args[5] as int : 10
 
 DesignTimeArtifact designTimeArtifact = new DesignTimeArtifact('https', host_tmn, 443, user, pw)
+// TODO - before deploying, check timestamp of latest design time, and also runtime - do not deploy if latest design time already deployed
+
 designTimeArtifact.deploy(iFlowId)
 
-println '[INFO] Checking deployment status'
+println "[INFO] Checking deployment status every ${delayLength} seconds up to ${maxCheckLimit} times"
 RuntimeArtifact runtimeArtifact = new RuntimeArtifact('https', host_tmn, 443, user, pw)
 Boolean deploymentComplete = false
+int checkCounter = 0
 while (!deploymentComplete) {
-    TimeUnit.SECONDS.sleep(30)
+    TimeUnit.SECONDS.sleep(delayLength)
     def status = runtimeArtifact.getStatus(iFlowId)
     println "[INFO] Current IFlow status = ${status}"
     if (status != 'STARTING') {
@@ -33,5 +38,10 @@ while (!deploymentComplete) {
             println "[ERROR] Error message = ${errorMessage}"
             System.exit(1)
         }
+    }
+    checkCounter++
+    if (checkCounter == maxCheckLimit && status != 'STARTED') {
+        println "[ERROR] IFlow status remained in ${status} after ${maxCheckLimit} checks"
+        System.exit(1)
     }
 }
