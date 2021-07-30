7e36b29274a4346f4eaf9a1e9d0675995ca79339
engswee <engswee@users.noreply.github.com>
2021-07-31 00:04:21 +0200

    Fix copyDirectory if source ends with /

--- src/main/groovy/io/github/engswee/flashpipe/cpi/util/FileUtility.groovy
+++ src/main/groovy/io/github/engswee/flashpipe/cpi/util/FileUtility.groovy
@@ -12,20 +12,21 @@ class FileUtility {
     static Logger logger = LoggerFactory.getLogger(FileUtility)
 
     static void removeCommentsFromFile(File inputFile) {
-        logger.debug("[INFO] Removing comments on ${inputFile}")
+        logger.debug("Removing comments on ${inputFile}")
         String fileContent = inputFile.getText('UTF-8')
         String updatedContent = fileContent.replaceAll(/#.*\r?\n/, '')
         inputFile.setText(updatedContent, 'UTF-8')
     }
 
     static void copyDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation) throws IOException {
-        logger.debug("[INFO] Copying directory from ${sourceDirectoryLocation} to ${destinationDirectoryLocation}")
-        Files.walk(Paths.get(sourceDirectoryLocation)).forEach({ source ->
-            Path destination = Paths.get(destinationDirectoryLocation, source.toString().substring(sourceDirectoryLocation.length()))
+        logger.debug("Copying directory from ${sourceDirectoryLocation} to ${destinationDirectoryLocation}")
+        Path sourceDirPath = Paths.get(sourceDirectoryLocation)
+        Files.walk(sourceDirPath).forEach({ source ->
+            Path destination = Paths.get(destinationDirectoryLocation, source.toString().substring(sourceDirPath.toString().length()))
             Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING)
         })
     }
-    
+
     static void replaceDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation) throws IOException {
         new File(destinationDirectoryLocation).deleteDir()
         copyDirectory(sourceDirectoryLocation, destinationDirectoryLocation)
