dde3772c0e76c978c777a5f3b6075465f0fa816f
engswee <engswee@users.noreply.github.com>
2021-09-12 21:04:00 +0200

    Update SimulatorSpec to use MockExpecation class

--- src/test/groovy/io/github/engswee/flashpipe/cpi/simulation/SimulatorSpec.groovy
+++ src/test/groovy/io/github/engswee/flashpipe/cpi/simulation/SimulatorSpec.groovy
@@ -2,13 +2,11 @@ package io.github.engswee.flashpipe.cpi.simulation
 
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+import io.github.engswee.flashpipe.cpi.util.MockExpectation
 import io.github.engswee.flashpipe.http.HTTPExecuter
 import io.github.engswee.flashpipe.http.HTTPExecuterApacheImpl
 import io.github.engswee.flashpipe.http.HTTPExecuterException
-import org.mockserver.client.MockServerClient
 import org.mockserver.integration.ClientAndServer
-import org.mockserver.model.HttpRequest
-import org.mockserver.model.HttpResponse
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -18,17 +16,23 @@ class SimulatorSpec extends Specification {
     ClientAndServer mockServer
     @Shared
     HTTPExecuter httpExecuter
+    @Shared
+    Simulator simulator
 
     final static String LOCALHOST = 'localhost'
-    MockServerClient mockServerClient
+    MockExpectation mockExpectation
 
     def setupSpec() {
         mockServer = ClientAndServer.startClientAndServer(9443)
         httpExecuter = HTTPExecuterApacheImpl.newInstance('http', LOCALHOST, 9443, 'dummy', 'dummy')
+        simulator = new Simulator(httpExecuter)
     }
 
     def setup() {
-        mockServerClient = new MockServerClient(LOCALHOST, 9443)
+        this.mockExpectation = MockExpectation.newInstance(LOCALHOST, 9443)
+    }
+
+    def cleanup() {
         mockServer.reset()
     }
 
@@ -38,17 +42,7 @@ class SimulatorSpec extends Specification {
 
     def 'Test token in GET CSRF Token call'() {
         given: 'Token provided in header of successful call'
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath('/itspaces/api/1.0/workspace')
-                .withHeader('x-csrf-token', 'fetch')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(200)
-                .withHeader('x-csrf-token', '50B5187CDE58A345C8A713959F9A4893')
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and: 'Simulator is instantiated with HTTPExecuter'
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.setCSRFTokenExpectation('/itspaces/api/1.0/workspace', '50B5187CDE58A345C8A713959F9A4893')
 
         when: 'CSRF Token is retrieved'
         def token = simulator.getCSRFToken()
@@ -59,16 +53,7 @@ class SimulatorSpec extends Specification {
 
     def 'GET CSRF Token call failed'() {
         given:
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath('/itspaces/api/1.0/workspace')
-                .withHeader('x-csrf-token', 'fetch')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(403)
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and:
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.setCSRFTokenExpectation('/itspaces/api/1.0/workspace', '50B5187CDE58A345C8A713959F9A4893', 403)
 
         when:
         simulator.getCSRFToken()
@@ -80,17 +65,7 @@ class SimulatorSpec extends Specification {
 
     def 'Test iFlow and package GUID in GET iFlow Artifact call'() {
         given: 'JSON response is provided in body of successful call'
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/odata/1.0/workspace.svc/Artifacts(Name='JSONToJSONTransformation',Type='IFlow')")
-                .withQueryStringParameter('$expand', 'ContentPackages')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(200)
-                .withBody(this.getClass().getResource('/test-data/Simulation/iFlowIDResponse.json').getText('UTF-8'))
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and: 'Simulator is instantiated with HTTPExecuter'
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/odata/1.0/workspace.svc/Artifacts(Name='JSONToJSONTransformation',Type='IFlow')", [:], ['$expand': 'ContentPackages'], 200, this.getClass().getResource('/test-data/Simulation/iFlowIDResponse.json').getText('UTF-8'), [:])
 
         when: 'iFlow and package GUID are retrieved'
         Map ids = simulator.getIFlowGuid('JSONToJSONTransformation')
@@ -104,16 +79,7 @@ class SimulatorSpec extends Specification {
 
     def 'GET IFlow GUID call failed'() {
         given:
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/odata/1.0/workspace.svc/Artifacts(Name='JSONToJSONTransformation',Type='IFlow')")
-                .withQueryStringParameter('$expand', 'ContentPackages')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(403)
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and:
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/odata/1.0/workspace.svc/Artifacts(Name='JSONToJSONTransformation',Type='IFlow')", [:], ['$expand': 'ContentPackages'], 403, '', [:])
 
         when:
         simulator.getIFlowGuid('JSONToJSONTransformation')
@@ -125,16 +91,7 @@ class SimulatorSpec extends Specification {
 
     def 'Test iFlow Model in GET iFlow Model call'() {
         given: 'JSON response is provided in body of successful call'
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy")
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(200)
-                .withBody(this.getClass().getResource('/test-data/Simulation/iFlowModelResponse.json').getText('UTF-8'))
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and: 'Simulator is instantiated with HTTPExecuter'
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy", 200, this.getClass().getResource('/test-data/Simulation/iFlowModelResponse.json').getText('UTF-8'))
 
         when: 'iFlow ID is retrieved'
         def model = simulator.getIFlowModel('6dbcfdfc969749f581bb5ee89b15f1a2', '19030a788e3f4efd94beea9217d6804a')
@@ -145,15 +102,7 @@ class SimulatorSpec extends Specification {
 
     def 'GET IFlow Model call failed'() {
         given:
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy")
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(403)
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and:
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy", 403, '')
 
         when:
         simulator.getIFlowModel('6dbcfdfc969749f581bb5ee89b15f1a2', '19030a788e3f4efd94beea9217d6804a')
@@ -199,17 +148,7 @@ class SimulatorSpec extends Specification {
 
     def 'Test incomplete execution in GET simulation results call'() {
         given: 'JSON response is provided in body of successful call'
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5")
-                .withQueryStringParameter('id', 'dummy')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(200)
-                .withBody(this.getClass().getResource('/test-data/Simulation/getTestStartedResponse.json').getText('UTF-8'))
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and: 'Simulator is instantiated with HTTPExecuter'
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5", [:], ['id': 'dummy'], 200, this.getClass().getResource('/test-data/Simulation/getTestStartedResponse.json').getText('UTF-8'), [:])
 
         when: 'Simulation result is queried'
         def result = simulator.querySimulationResult('6dbcfdfc969749f581bb5ee89b15f1a2', '19030a788e3f4efd94beea9217d6804a', 'f52b4c67-befb-41df-82d7-89d7771bfbb5', 'SequenceFlow_6')
@@ -220,17 +159,7 @@ class SimulatorSpec extends Specification {
 
     def 'Test completed execution in GET simulation results call'() {
         given: 'JSON response is provided in body of successful call'
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5")
-                .withQueryStringParameter('id', 'dummy')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(200)
-                .withBody(this.getClass().getResource('/test-data/Simulation/getTestSuccessResponse.json').getText('UTF-8'))
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and: 'Simulator is instantiated with HTTPExecuter'
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5", [:], ['id': 'dummy'], 200, this.getClass().getResource('/test-data/Simulation/getTestSuccessResponse.json').getText('UTF-8'), [:])
 
         when: 'Simulation result is queried'
         Map result = simulator.querySimulationResult('6dbcfdfc969749f581bb5ee89b15f1a2', '19030a788e3f4efd94beea9217d6804a', 'f52b4c67-befb-41df-82d7-89d7771bfbb5', 'SequenceFlow_6')
@@ -241,17 +170,7 @@ class SimulatorSpec extends Specification {
 
     def 'Test execution loop for GET simulation results call'() {
         given: 'JSON response is provided in body of successful call'
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5")
-                .withQueryStringParameter('id', 'dummy')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(200)
-                .withBody(this.getClass().getResource('/test-data/Simulation/getTestSuccessResponse.json').getText('UTF-8'))
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and: 'Simulator is instantiated with HTTPExecuter'
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5", [:], ['id': 'dummy'], 200, this.getClass().getResource('/test-data/Simulation/getTestSuccessResponse.json').getText('UTF-8'), [:])
 
         when: 'Simulation output is retrieved'
         Map result = simulator.getSimulationOutput('6dbcfdfc969749f581bb5ee89b15f1a2', '19030a788e3f4efd94beea9217d6804a', 'f52b4c67-befb-41df-82d7-89d7771bfbb5', 'SequenceFlow_6', 1)
@@ -262,16 +181,7 @@ class SimulatorSpec extends Specification {
 
     def 'Query simulation result call failed'() {
         given:
-        def httpRequest = HttpRequest.request()
-                .withMethod('GET')
-                .withPath("/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5")
-                .withQueryStringParameter('id', 'dummy')
-        def httpResponse = HttpResponse.response()
-                .withStatusCode(403)
-        mockServerClient.when(httpRequest).respond(httpResponse)
-
-        and:
-        Simulator simulator = new Simulator(httpExecuter)
+        this.mockExpectation.set('GET', "/itspaces/api/1.0/workspace/6dbcfdfc969749f581bb5ee89b15f1a2/artifacts/19030a788e3f4efd94beea9217d6804a/entities/19030a788e3f4efd94beea9217d6804a/iflows/dummy/simulations/f52b4c67-befb-41df-82d7-89d7771bfbb5", [:], ['id': 'dummy'], 403, '', [:])
 
         when:
         simulator.querySimulationResult('6dbcfdfc969749f581bb5ee89b15f1a2', '19030a788e3f4efd94beea9217d6804a', 'f52b4c67-befb-41df-82d7-89d7771bfbb5', 'SequenceFlow_6')
