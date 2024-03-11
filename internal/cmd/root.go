67d19445251c452bcae5f0c08dc8e651fa0b9655
Eng Swee Yeoh <engswee@users.noreply.github.com>
2024-03-11 14:20:20 +0000

    Prepare for next development iteration - Set version to 3.2.2
    - update actions to latest using node20

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -13,7 +13,7 @@ import (
 )
 
 func NewCmdRoot() *cobra.Command {
-	var version = "3.2.1" // FLASHPIPE_VERSION
+	var version = "3.2.2" // FLASHPIPE_VERSION
 
 	// rootCmd represents the base command when called without any subcommands
 	rootCmd := &cobra.Command{
